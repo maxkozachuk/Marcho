c8f2094c72e24b1495d499042eec9ee36361a44c
Max Kozachuk <maxkozachuk1@gmail.com>
2023-08-26 19:32:03 +0300

    feat: add shop.html adaptive

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
 $(function () {
 
+  $('.shop__filters-btn').on('click', function () {
+    $('.filters').slideToggle();
+  });
+
   $('.menu__btn').on('click', function () {
     $('.menu__list').toggleClass('menu__list--active');
     $(this).toggleClass('menu__btn--active');
